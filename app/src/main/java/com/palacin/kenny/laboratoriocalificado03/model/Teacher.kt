197ecdfe79ff74e8a957f9d5d64c288a7f1ab1bd
Kenny2006-PE <kenny.palacin@tecsup.edu.pe>
2024-11-16 09:45:13 -0500

    Aplicativo funcionando correctamente

---
+++ app/src/main/java/com/palacin/kenny/laboratoriocalificado03/model/Teacher.kt
@@ -0,0 +1,9 @@
+package com.palacin.kenny.laboratoriocalificado03.model
+
+data class Teacher(
+    val name: String,
+    val lastName: String,
+    val phoneNumber: String,
+    val email: String,
+    val imageUrl: String
+)
